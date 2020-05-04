3c903bbd40705fcd5adf86750dbf8a517c02ef78
Rushad Mehta <rushadmehta16@gmail.com>
2020-05-04 17:57:10 +0530

    Modified path planning algorithm

---
+++ path_mod.py
@@ -0,0 +1,47 @@
+from collections import namedtuple, deque
+import adj_mat
+ 
+ 
+inf = float('inf')
+Edge = namedtuple('Edge', ['start', 'end', 'cost'])
+ 
+class Graph():
+    def __init__(self, edges):
+        self.edges = [Edge(*edge) for edge in edges]
+        # print(dir(self.edges[0]))
+        self.vertices = {e.start for e in self.edges} | {e.end for e in self.edges}
+ 
+    def dijkstra(self, source, dest):
+        assert source in self.vertices
+        dist = {vertex: inf for vertex in self.vertices}
+        previous = {vertex: None for vertex in self.vertices}
+        dist[source] = 0
+        q = self.vertices.copy()
+        neighbours = {vertex: set() for vertex in self.vertices}
+        for start, end, cost in self.edges:
+            neighbours[start].add((end, cost))
+        #print(neighbours)
+ 
+        while q:
+            # print(q)
+            u = min(q, key=lambda vertex: dist[vertex])
+            q.remove(u)
+            if dist[u] == inf or u == dest:
+                break
+            for v, cost in neighbours[u]:
+                alt = dist[u] + cost
+                if alt < dist[v]:
+                    dist[v] = alt
+                    previous[v] = u
+        #print(previous)
+        s, u = deque(), dest
+        while previous[u]:
+            s.appendleft(u)
+            u = previous[u]
+        s.appendleft(u)
+        return s
+ 
+_,node_rel = adj_mat.adj_mat_coor(8) 
+graph = Graph(node_rel)
+
+print(graph.dijkstra(56,63))
